8df592fa536024c5b47eb4cfe56ee3f07f96d963
Firzi <firziawan20@gmail.com>
2023-08-08 05:15:15 +0700

    add: API property swagger

--- src/auth/dtos/create.user.dto.ts
+++ src/auth/dtos/create.user.dto.ts
@@ -1,5 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty()
   name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
   password: string;
 }
--- src/auth/dtos/login.user.dto.ts
+++ src/auth/dtos/login.user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsEmail } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
